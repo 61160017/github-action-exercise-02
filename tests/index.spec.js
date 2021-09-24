09b2c296152b98e6b491d6242f5dd757c502db82
61160017 <61160017@go.buu.ac.th>
2021-09-24 23:33:54 +0700

    My homework#2 61160017: Test against GET /users

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
 const app = require('../index')
 
-describe('GET /', () => {
+describe('GET /users', () => {
     it('responds with "No user in the system"', (done) => {
-        request(app).get('/').expect('No user in the system', done)
+        request(app).get('/users').expect('No user in the system', done)
     })
 })
